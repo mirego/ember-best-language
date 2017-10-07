817e2ce5ac3ecc2607bf8dc4c73d94f904236998
Charles Demers <charles.demers6@gmail.com>
2017-10-07 10:28:35 -0400

    Fix non-FastBoot apps and add test scenarios to test it

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,94 +1,65 @@
 /* eslint-env node */
 
-'use strict';
+module.exports = function() {
+  return {
+    command: 'ember test',
+    useYarn: true,
+    scenarios: [
+      {
+        name: 'ember-lts-without-fastboot',
+        command: 'ember test --filter "when running inside FastBoot" --invert',
+        npm: {
+          devDependencies: {
+            'ember-source': 'lts',
+            'ember-cli-fastboot': null
+          }
+        }
+      },
+      {
+        name: 'ember-latest-without-fastboot',
+        command: 'ember test --filter "when running inside FastBoot" --invert',
+        npm: {
+          devDependencies: {
+            'ember-source': 'latest',
+            'ember-cli-fastboot': null
+          }
+        }
+      },
+      {
+        name: 'ember-beta-without-fastboot',
+        command: 'ember test --filter "when running inside FastBoot" --invert',
+        npm: {
+          devDependencies: {
+            'ember-source': 'beta',
+            'ember-cli-fastboot': null
+          }
+        }
+      },
+      {
+        name: 'ember-lts-with-fastboot',
+        npm: {
+          devDependencies: {
+            'ember-source': 'lts'
+          }
+        }
+      },
+      {
+        name: 'ember-latest-with-fastboot',
 
-module.exports = {
-  scenarios: [
-    {
-      name: 'ember-lts-2.4',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#lts-2-4'
-        },
-        resolutions: {
-          ember: 'lts-2-4'
+        npm: {
+          devDependencies: {
+            'ember-source': 'latest'
+          }
         }
       },
-      npm: {
-        devDependencies: {
-          'ember-source': null
+      {
+        name: 'ember-beta-with-fastboot',
+        npm: {
+          devDependencies: {
+            'ember-source': 'beta'
+          }
         }
       }
-    },
-    {
-      name: 'ember-lts-2.8',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#lts-2-8'
-        },
-        resolutions: {
-          ember: 'lts-2-8'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#release'
-        },
-        resolutions: {
-          ember: 'release'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#beta'
-        },
-        resolutions: {
-          ember: 'beta'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          ember: 'components/ember#canary'
-        },
-        resolutions: {
-          ember: 'canary'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-default',
-      npm: {
-        devDependencies: {}
-      }
-    }
-  ]
+    ]
+  };
 };
